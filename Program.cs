2be85270892e6dc456decfd639c8a97f2b3f0dbb
MG Creations <info@mgcreations.co.za>
2020-06-21 20:33:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CheckLowAndHighRates
+{
+    class Program
+    {
+        static void Main()
+        {
+            const double LOW_RATE = 5.65;
+            const double HIGH_RATE = 49.99;
+
+            double hourlyRatePay;
+
+            Console.Write("Enter the hourly rate of pay:");
+            hourlyRatePay = Convert.ToDouble(Console.ReadLine());
+
+            if (hourlyRatePay <= LOW_RATE)
+                Console.WriteLine("Error: The rate of pay {0} is lower than {1}",
+                    hourlyRatePay.ToString("C") ,LOW_RATE.ToString("C"));
+            else if (hourlyRatePay >= HIGH_RATE)
+                Console.WriteLine("Error: The rate of pay {0} is higher than {1}",
+                    hourlyRatePay.ToString("C"), HIGH_RATE.ToString("C"));
+            else
+                Console.WriteLine("Rate of pay entered: {0}", hourlyRatePay.ToString("C"));
+        }
+    }
+}
